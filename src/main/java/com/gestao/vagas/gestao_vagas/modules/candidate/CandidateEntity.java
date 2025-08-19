a2017afa6c97a37cd80d184270d6edcf629a08f1
Larissa Menezes <larissasezenem@gmail.com>
2025-08-19 19:04:20 -0300

    feat: initial project setup with Candidate entity and controller

---
+++ src/main/java/com/gestao/vagas/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -0,0 +1,29 @@
+package com.gestao.vagas.gestao_vagas.modules.candidate;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.UUID;
+
+@Data //getters and setters
+public class CandidateEntity {
+
+    private UUID id;
+    private String name;
+
+    //validations
+    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não deve conter espaço!")
+    private String username;
+
+    @Email(message = "O campo [e-mail] deve conter um e-mail válido!")
+    private String email;
+
+    @Length(min = 10, max = 100) //password
+    private String password;
+    private String description;
+    private String curriculum;
+
+
+}
