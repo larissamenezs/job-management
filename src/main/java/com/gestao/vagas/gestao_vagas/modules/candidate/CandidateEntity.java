7f1d4091b6a1b0d4c79fa2b73a7e86237ebea231
Larissa Menezes <larissasezenem@gmail.com>
2025-08-28 18:57:13 -0300

    feat(candidate): update Candidate entity, repository, controller and project configs (docker, pom.xml, properties)

--- src/main/java/com/gestao/vagas/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/gestao/vagas/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,17 +1,27 @@
 package com.gestao.vagas.gestao_vagas.modules.candidate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data //getters and setters
+@Entity(name = "candidate")
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID) //Creates ID automatically
     private UUID id;
+
     private String name;
 
     //validations
@@ -27,5 +37,7 @@ public class CandidateEntity {
     private String description;
     private String curriculum;
 
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
--- src/main/java/com/gestao/vagas/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/gestao/vagas/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -2,21 +2,24 @@ package com.gestao.vagas.gestao_vagas.modules.candidate.controllers;
 
 import com.gestao.vagas.gestao_vagas.modules.candidate.CandidateEntity;
 
+import com.gestao.vagas.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
+    @Autowired //manage my repository
+    private CandidateRepository candidateRepository;
+
     @PostMapping("/")
-    public void create( @Valid @RequestBody CandidateEntity candidateEntity) {
-        System.out.println("Candidato");
-        System.out.println(candidateEntity.getEmail());
+    public CandidateEntity create( @Valid @RequestBody CandidateEntity candidateEntity) {
+        return this.candidateRepository.save(candidateEntity);
     }
 
 }
