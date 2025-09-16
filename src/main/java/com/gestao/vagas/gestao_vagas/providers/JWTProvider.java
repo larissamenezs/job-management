8c55f2d858d025d679afab05a9eab22a2b02d328
Larissa Menezes <larissasezenem@gmail.com>
2025-09-16 18:27:33 -0300

    feat(jwt): inject secret key with @Value and add stack trace logging

--- src/main/java/com/gestao/vagas/gestao_vagas/providers/JWTProvider.java
+++ src/main/java/com/gestao/vagas/gestao_vagas/providers/JWTProvider.java
@@ -3,11 +3,13 @@ package com.gestao.vagas.gestao_vagas.providers;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JWTProvider {
 
+    @Value("${security.token.secret}")
     private String secretKey;
 
     public String validateToken(String token) {
@@ -22,6 +24,7 @@ public class JWTProvider {
                     .getSubject();
             return subject;
         } catch (JWTVerificationException ex) {
+            ex.printStackTrace();
             return "";
         }
     }
