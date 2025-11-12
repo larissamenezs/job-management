c3427ae146d565b9188c7bf35fd668eff0907a8c
Larissa Menezes <larissasezenem@gmail.com>
2025-11-12 17:45:21 -0300

    refactor(candidate): update controller and use case to handle authentication flow

--- src/main/java/com/gestao/vagas/gestao_vagas/providers/JWTCandidateProvider.java
+++ src/main/java/com/gestao/vagas/gestao_vagas/providers/JWTCandidateProvider.java
@@ -8,9 +8,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
-public class JWTProvider {
+public class JWTCandidateProvider {
 
-    @Value("${security.token.secret}")
+    @Value("${security.token.secret.candidate}")
     private String secretKey;
 
     public DecodedJWT validateToken(String token) {
@@ -21,12 +21,12 @@ public class JWTProvider {
         Algorithm algorithm = Algorithm.HMAC256(secretKey);
 
         try {
-            var tokenDecod = JWT.require(algorithm)
+            var tokenDecoded = JWT.require(algorithm)
                     .build()
                     .verify(token);
-            return tokenDecod;
-        } catch (JWTVerificationException ex) {
-            ex.printStackTrace();
+            return tokenDecoded;
+        } catch (JWTVerificationException e) {
+            e.printStackTrace();
             return null;
         }
     }
--- src/main/java/com/gestao/vagas/gestao_vagas/security/SecurityCandidateFilter.java
+++ src/main/java/com/gestao/vagas/gestao_vagas/security/SecurityCandidateFilter.java
@@ -1,24 +1,27 @@
 package com.gestao.vagas.gestao_vagas.security;
 
+import com.gestao.vagas.gestao_vagas.providers.JWTCandidateProvider;
 import com.gestao.vagas.gestao_vagas.providers.JWTProvider;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.cassandra.CassandraProperties;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
 import java.io.IOException;
 import java.util.Collections;
 
 @Component
-public class SecurityFilter extends OncePerRequestFilter {
+public class SecurityCandidateFilter extends OncePerRequestFilter {
 
     @Autowired
-    private JWTProvider jwtProvider;
+    private JWTCandidateProvider jwtProvider;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -27,25 +30,27 @@ public class SecurityFilter extends OncePerRequestFilter {
         //SecurityContextHolder.getContext().setAuthentication(null);
         String header = request.getHeader("Authorization");
 
-        if(request.getRequestURI().startsWith("/company")) {
+        if(request.getRequestURI().startsWith("/candidate")) {
 
-            if (header != null) {
+            if(header != null) {
                 var token = this.jwtProvider.validateToken(header);
-                if (token == null) {
+
+                if(token == null) {
                     response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
                     return;
                 }
-
+                request.setAttribute("candidate_id", token.getSubject());
                 var roles = token.getClaim("roles").asList(Object.class);
-                var grants = roles.stream()
-                                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.toString().toUpperCase()))
-                                        .toList();
 
-                request.setAttribute("company_id", token.getSubject());
-                UsernamePasswordAuthenticationToken auth =
-                        new UsernamePasswordAuthenticationToken(token.getSubject(), null,
-                                grants);
+                var grants = roles.stream()
+                        .map(
+                            role -> new SimpleGrantedAuthority("ROLE_" + role.toString().toUpperCase()))
+                        .toList();
+
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(token.getSubject(), null,
+                        grants);
                 SecurityContextHolder.getContext().setAuthentication(auth);
+
             }
         }
         filterChain.doFilter(request, response);
