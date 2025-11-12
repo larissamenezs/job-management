238c837355bb127675af883c503bb09f9281d11b
Larissa Menezes <larissasezenem@gmail.com>
2025-11-12 18:03:10 -0300

    feat(candidate): improve authentication and document endpoints with Swagger

--- src/main/java/com/gestao/vagas/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
+++ src/main/java/com/gestao/vagas/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -41,15 +41,17 @@ public class AuthCandidateUseCase {
         }
 
         Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        var expiresIn = Instant.now().plus(Duration.ofMinutes(10));
         var token = JWT.create()
                 .withIssuer("javagas")
                 .withSubject(candidate.getId().toString())
-                .withClaim("roles", Arrays.asList("candidate"))
-                .withExpiresAt(Instant.now().plus(Duration.ofMinutes(10)))
+                .withClaim("roles", Arrays.asList("CANDIDATE"))
+                .withExpiresAt(expiresIn)
                 .sign(algorithm);
 
         var authCandidateResponse = AuthCandidateResponseDTO.builder()
                 .access_token(token)
+                .expires_in(expiresIn.toEpochMilli())
                 .build();
 
         return authCandidateResponse;
