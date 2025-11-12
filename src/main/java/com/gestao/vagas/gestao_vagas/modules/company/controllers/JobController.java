bec0557bdd7149abbd500cf94b89d648645761d8
Larissa Menezes <larissasezenem@gmail.com>
2025-11-12 18:18:14 -0300

    feat(job): add security restrictions and Swagger documentation to job endpoints

--- src/main/java/com/gestao/vagas/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/com/gestao/vagas/gestao_vagas/modules/company/controllers/JobController.java
@@ -3,10 +3,19 @@ package com.gestao.vagas.gestao_vagas.modules.company.controllers;
 import com.gestao.vagas.gestao_vagas.modules.company.dto.CreateJobDTO;
 import com.gestao.vagas.gestao_vagas.modules.company.entities.JobEntity;
 import com.gestao.vagas.gestao_vagas.modules.company.useCases.CreateJobUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,13 +24,24 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/job")
+@RequestMapping("/company/job")
 public class JobController {
 
     @Autowired
     private CreateJobUseCase createJobUseCase;
 
     @PostMapping("/")
+    @PreAuthorize("hasRole('COMPANY')")
+    @Tag(name = "Vagas", description = "Informações das vagas")
+    @Operation(summary = "Cadastro de vagas",
+
+            description = "Esta função é responsável por cadastrar as vagas dentro da empresa")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(schema = @Schema(implementation = JobEntity.class))
+            })
+    })
+    @SecurityRequirement(name = "jwt_auth")
     public JobEntity create (@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
         var companyId = request.getAttribute("company_id");
 
--- src/main/java/com/gestao/vagas/gestao_vagas/modules/company/repositories/JobRepository.java
+++ src/main/java/com/gestao/vagas/gestao_vagas/modules/company/repositories/JobRepository.java
@@ -3,7 +3,14 @@ package com.gestao.vagas.gestao_vagas.modules.company.repositories;
 import com.gestao.vagas.gestao_vagas.modules.company.entities.JobEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface JobRepository extends JpaRepository<JobEntity, UUID> {
+
+    // "contains - LIKE"
+
+    // Select * from job where description like %filter%
+
+    List<JobEntity> findByDescriptionContainingIgnoreCase(String filter);
 }
