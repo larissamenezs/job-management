c0ea7fd853e79e1390589a6fd056e96d77d2959b
Larissa Menezes <larissasezenem@gmail.com>
2025-11-12 18:16:35 -0300

    feat(company-auth): implement authentication flow and response DTO for company login

--- src/main/java/com/gestao/vagas/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/com/gestao/vagas/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -3,6 +3,7 @@ package com.gestao.vagas.gestao_vagas.modules.company.useCases;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.gestao.vagas.gestao_vagas.modules.company.dto.AuthCompanyDTO;
+import com.gestao.vagas.gestao_vagas.modules.company.dto.AuthCompanyResponseDTO;
 import com.gestao.vagas.gestao_vagas.modules.company.repositories.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Arrays;
 
 @Service
 public class AuthCompanyUseCase {
@@ -26,23 +28,32 @@ public class AuthCompanyUseCase {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public String execute(AuthCompanyDTO authCompanyDTO) {
+    public AuthCompanyResponseDTO execute(AuthCompanyDTO authCompanyDTO) {
 
         var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(
-                () -> {
-                    throw new UsernameNotFoundException("Username or password incorrect");
-                });
+                () -> new UsernameNotFoundException("Username or password incorrect"));
 
         var passwordMatches = this.passwordEncoder.matches(authCompanyDTO.getPassword(), company.getPassword());
 
         if (!passwordMatches) {
             throw new BadCredentialsException("Invalid Password");
         }
+
         Algorithm algorithm = Algorithm.HMAC256(secretKey);
-        var token = JWT.create().withIssuer("javagas")
-                .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
+        var expiresIn = Instant.now().plus(Duration.ofHours(2));
+
+        var token = JWT.create()
+                .withIssuer("javagas")
+                .withExpiresAt(expiresIn)
                 .withSubject(company.getId().toString())
+                .withClaim("roles", Arrays.asList("COMPANY"))
                 .sign(algorithm);
-        return token;
+
+        var authCompanyResponseDTO = AuthCompanyResponseDTO.builder()
+                .access_token(token)
+                .expires_in(expiresIn.toEpochMilli())
+                .build();
+
+        return authCompanyResponseDTO;
     }
 }
