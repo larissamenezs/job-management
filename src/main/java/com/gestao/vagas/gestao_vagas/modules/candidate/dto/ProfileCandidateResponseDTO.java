adef0cba5b5b833ce172555e2ef4f0e19aff5559
Larissa Menezes <larissasezenem@gmail.com>
2025-11-12 17:38:19 -0300

    feat(candidate): add authentication controller, profile and job listing use cases

---
+++ src/main/java/com/gestao/vagas/gestao_vagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
@@ -0,0 +1,31 @@
+package com.gestao.vagas.gestao_vagas.modules.candidate.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProfileCandidateResponseDTO {
+
+    @Schema (example = "Desenvolvedora Java")
+    private String description;
+
+    @Schema (example = "Maria")
+    private String username;
+
+    @Schema (example = "maria@gmail.com")
+    private String email;
+
+    @Schema (example = "Maria de Souza")
+    private String name;
+
+    private UUID id;
+
+}
