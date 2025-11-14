08d4e8d1514712c2c17de96c79ae9a30ede4e272
Larissa Menezes <larissasezenem@gmail.com>
2025-11-14 12:33:36 -0300

    feat(exception): add custom exceptions for job and user not found
    Introduced JobNotFoundException and UserNotFoundException to improve error handling across modules

---
+++ src/main/java/com/gestao/vagas/gestao_vagas/exceptions/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.gestao.vagas.gestao_vagas.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException() {
+        super("User not found");
+    }
+}
